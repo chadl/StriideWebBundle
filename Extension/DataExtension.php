bbaab4529fb467408e7744c182c25a720eb6fb24
Chad Lindstrom <chad.lindstrom@gmail.com>
2012-06-28 22:51:33 -0700

    more extensions

--- Extension/DataExtension.php
+++ Extension/DataExtension.php
@@ -6,8 +6,25 @@ class DataExtension extends \Twig_Extension
   public function getFilters()
   {
     return array(
-      'count' => new \Twig_Filter_Method($this, 'countItems')
+      'count' => new \Twig_Filter_Method($this, 'countItems'),
+      'boolString' => new \Twig_Filter_Method($this, 'boolString'),
     );
+  }
+
+  public function boolString($object)
+  {
+    if(is_null($object))
+    {
+      return "";
+    }
+    else if ($object === true)
+    {
+      return "Yes";
+    }
+    else
+    {
+      return "No";
+    }
   }
 
   /**
